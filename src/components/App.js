dc2421e3302e95e03a6877c6d5372706403eec7e
muskan04yadav <muskyad@gmail.com>
2024-12-10 13:28:34 +0530

    added files

---
+++ src/components/App.js
@@ -0,0 +1,52 @@
+import { db } from '../firebase-config'
+import SignUp from './SignUp'
+import Login from './Login'
+import DashBoard from './DashBoard'
+import Infant from './Infant'
+import PreSchool from './PreSchool'
+import SchoolAge from './SchoolAge'
+import ForgotPassword from './ForgotPassword'
+import { AuthProvider } from '../contexts/AuthContext'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useAuth } from '../contexts/AuthContext'
+
+function App() {
+  const { currentUser } = useAuth()
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path='/*'
+            element={currentUser !== null ? <DashBoard /> : <Login />}
+          />
+          <Route
+            path='/infant'
+            element={currentUser !== null ? <Infant /> : <Login />}
+          />
+          <Route
+            path='/preschool'
+            element={currentUser !== null ? <PreSchool /> : <Login />}
+          />
+          <Route
+            path='/school-age'
+            element={currentUser !== null ? <SchoolAge /> : <Login />}
+          />
+
+          <Route
+            path='/signup'
+            element={currentUser !== null ? <DashBoard /> : <SignUp />}
+          />
+          <Route
+            path='/login'
+            element={currentUser !== null ? <DashBoard /> : <Login />}
+          />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
